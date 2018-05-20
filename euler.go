e43d076ae977dcf33811757c3cf8958c79964da6
jleech <eveleech@gmail.com>
2018-05-20 23:12:05 +0000

    formatted by gofmt

--- euler.go
+++ euler.go
@@ -3,17 +3,16 @@ package main
 import (
 	"fmt"
 	"math"
-	)
-
+)
 
 func getPrimes(N int) []int {
 	// Eratosphen algo
 	isComposite := make([]bool, N)
 	primes := []int{}
-	for i := 2; i < N; i ++ {
+	for i := 2; i < N; i++ {
 		if !isComposite[i] {
 			primes = append(primes, i)
-			for x := i+i; x < N; x += i {
+			for x := i + i; x < N; x += i {
 				isComposite[x] = true
 			}
 		}
@@ -21,14 +20,13 @@ func getPrimes(N int) []int {
 	return primes
 }
 
-
 func getPrimeFactors(n int, primes []int) map[int]float64 {
 	primeFactors := make(map[int]float64) // prime : power
 	for n != 1 {
-		for i := 0; i < len(primes); i ++ {
-			if n % primes[i] == 0 {
+		for i := 0; i < len(primes); i++ {
+			if n%primes[i] == 0 {
 				primeFactors[primes[i]] += 1.0
-				n = n/primes[i]
+				n = n / primes[i]
 				break
 			}
 		}
@@ -36,19 +34,19 @@ func getPrimeFactors(n int, primes []int) map[int]float64 {
 	return primeFactors
 }
 
-func eulerFunc(n int) int{
+func eulerFunc(n int) int {
 	eulerResult := 1.0
-	for prime, power := range getPrimeFactors(n, getPrimes(n)){
-			eulerResult *= math.Pow(float64(prime), power) - 
+	for prime, power := range getPrimeFactors(n, getPrimes(n)) {
+		eulerResult *= math.Pow(float64(prime), power) -
 			math.Pow(float64(prime), power-1)
-		}
+	}
 	return int(eulerResult)
 }
 
-func primeTest(value int, primeFactors map[int]int) bool{
+func primeTest(value int, primeFactors map[int]int) bool {
 	primeCheck := 1.0
 	for key, value := range primeFactors {
-    	primeCheck *= math.Pow(float64(key), float64(value))
+		primeCheck *= math.Pow(float64(key), float64(value))
 	}
 	return (value == int(primeCheck))
 }
